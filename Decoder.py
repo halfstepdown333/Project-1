d3564536a8161ebd214d7eb36b775a9c4ee4672b
Tavo <gortiz8@miners.utep.edu>
2019-09-01 13:49:10 -0600

    Tested program using the terminal and created the final test included in speechTest.txt

--- Decoder.py
+++ Decoder.py
@@ -5,7 +5,7 @@ import sys
 #modified extract from wordCountTest.py in repository
 # set input and output files
 if len(sys.argv) is not 3:
-    print("Correct usage: Decoder.py <input text file> <output file> ")
+    print("Correct usage: Decoder.py <input text file> <output file> ") #correct syntax for operating Decoder.py in terminal
     exit()
 
 userInput = sys.argv[1] #stores file name (input)
@@ -20,20 +20,20 @@ if not os.path.exists(userInput):
 
 with open(userInput, "r") as f:
     data = f.read()
-    clean = re.sub('[^A-Za-z0-9]+', ' ', data)
+    clean = re.sub('[^A-Za-z0-9]+', ' ', data) #removes punctuations and sets a space after the word
 
-words = clean.split()
+words = clean.split()   #splits the words into a list
 
-counts = dict()
+counts = {}   #space to save the words
 
 for word in words:
     if word in counts:
-        counts[word.lower()] += 1
+        counts[word.lower()] += 1  #Every time an element matches with another it updates
     else:
-        counts[word.lower()] = 1
+        counts[word.lower()] = 1   #Else word was only found once
 
 tf = open(outputFile, "w+")
-for x, y in sorted(counts.items()):
+for x, y in sorted(counts.items()):    #writes to file items of the dictionary 
     tf.write(str(x) + ' : ' + str(y) + '\n')
 
 tf.close()
